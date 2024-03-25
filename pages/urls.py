49782fd6be24eaa378bcb05bd11bd66654a5e6c7
KidaqrusFan <amberoliva98@gmail.com>
2024-03-25 22:06:26 +0100

    added links to pages

--- pages/urls.py
+++ pages/urls.py
@@ -4,6 +4,15 @@ from pages import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('detail/', views.detail, name='detail'),
+    path('detail1/', views.detail1, name='detail1'),
+    path('detail2/', views.detail2, name='detail2'),
+    path('detail3/', views.detail3, name='detail3'),
+    path('detail4/', views.detail4, name='detail4'),
+    path('detail5/', views.detail5, name='detail5'),
+    path('detail6/', views.detail6, name='detail6'),
+    path('about/', views.about, name='about'),
+    path('property/', views.property, name='property')
+
 ]
 
 
--- pages/views.py
+++ pages/views.py
@@ -8,5 +8,30 @@ def home(request):
 def detail(request):
     return render(request, "pages/detail.html", {})
 
+def detail1(request):
+     return render(request, "pages/detail1.html", {})
+
+
+def detail2(request):
+    return render(request, "pages/detail2.html", {})
+
+
+def detail3(request):
+    return render(request, "pages/detail3.html", {})
+
+def detail4(request):
+    return render(request, "pages/detail4.html", {})
+
+def detail5(request):
+    return render(request, "pages/detail5.html", {})
+
+def detail6(request):
+    return render(request, "pages/detail6.html", {})
+
+def about(request):
+    return render(request, "pages/about.html", {})
+
+def property(request):
+    return render(request, "pages/property.html", {})
 
 
