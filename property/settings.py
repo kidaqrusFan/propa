2470182b9f80623242e15d07a39e2d8464074c53
KidaqrusFan <amberoliva98@gmail.com>
2024-04-10 04:57:50 +0100

    almost done

--- property/settings.py
+++ property/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@_w!rlvmlcl^okb&jo_a3=nx=w%1&e$cusxbx%jc(7646%b2e$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
